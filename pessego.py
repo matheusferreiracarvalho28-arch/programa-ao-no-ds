0018f004043f082fc0493732401f3650f75cdd55
matheusferreiracarvalho28-arch <matheus.ferreira.carvalho28@escola.pr.gov.br>
2026-03-19 09:46:01 -0300

    inicio a função

--- pessego.py
+++ pessego.py
@@ -1,21 +1,25 @@
 a = int(input("digite o numero"))
 b = int(input("digite o numero"))
 pessego = True
-while pessego == True:
-  print ("1 - Soma \n 2 - divisão\n 3 - subtração \n 4 - multiplicação \n 0-sair")
-  op = input ("digite uma opção: ")
-  if op == "0": 
-    pessego = False
-  if op == "1":
-    resultado = a + b 
-    print (resultado)
-  if op == "2":
-    resultado = a / b
-    print (resultado)
-  if op == "3":
-    resultado = a - b
-    print (resultado)
-  if op == "4":
-    resultado = a * b
-    print (resultado)
-  
+def somar (a,b):
+  a+b 
+  print ()
+  while pessego == True:
+    print ("1 - Soma \n 2 - divisão\n 3 - subtração \n 4 - multiplicação \n 5  - porcentagem \n 0-sair")
+    op = input ("digite uma opção: ")
+    if op == "0": 
+      pessego = False
+    if op == "1":
+      somar(a,b)
+    if op == "2":
+      resultado = a / b
+      print (resultado)
+    if op == "3":
+      resultado = a - b
+      print (resultado)
+    if op == "4":
+      resultado = a * b
+      print (resultado)
+    if op == "5":
+      resultado = a % b
+      print (resultado)
