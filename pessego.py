cf80f4096db6d7bebe71c3332251f02f158022a7
matheusferreiracarvalho28-arch <matheus.ferreira.carvalho28@escola.pr.gov.br>
2026-03-19 09:20:08 -0300

    inicialização

---
+++ pessego.py
@@ -0,0 +1,19 @@
+a = int(input("digite o numero"))
+b = int(input("digite o numero"))
+pessego = True
+while pessego == True:
+  print ("1 - Soma \n 2 - divisão\n 3 - subtração \n 0-sair")
+  op = input ("digite uma opção: ")
+  if op == "0": 
+    pessego = False
+  if op == "1":
+    resultado = a + b 
+    print (resultado)
+  if op == "2":
+    resultado = a / b
+    print (resultado)
+  if op == "3":
+    resultado = a - b
+    print (resultado)
+
+  
